7ae28f3dc1b433a254ccd15ffdb9008ee657dfec
azamatnarzulloyev <azamatsabina1796@mail.ru>
2022-04-26 10:14:48 +0500

    push all

--- 14-dars.py
+++
@@ -1,7 +0,0 @@
-dostim = {
-    'ism':'murod',
-    'yoshi':'26',
-    'tu_yil':'1996',
-}
-# print(f" dostimning ismi {dostim['ism'] } ")
-print(dostim)
--- 15-dars.py
+++
@@ -1,8 +0,0 @@
-talaba_0 = {
-    'ism':'Murodjon',
-    'familya':'Narzulloyev',
-    'yoshi':'1995',
-    'manzili':'shofirkon',
-}
-print(talaba_0.items())
-print(talaba_0)
